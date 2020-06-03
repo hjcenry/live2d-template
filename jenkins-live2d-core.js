211b8b39ae0d1a5a40cf03f32773a87764e8f641
hjcenry <HJCenry921209>
2020-06-03 21:39:39 +0800

    update

--- jenkins-live2d-core.js
+++ jenkins-live2d-core.js
@@ -1,16 +1,37 @@
 var message_Path = 'http://172.16.1.98:8083/userContent/live2d-core';
 var home_Path = 'http://172.16.1.98:8083/';
+
+function chatUILogic() {
+    var showChatBtn = Q("<button class='chat-button'>聊天</button>");
+    Q('div#landlord').append(showChatBtn);
+    Q(showChatBtn).show();
+    var hideChatBtn = Q("<button class='chat-button'>不聊了</button>");
+    Q('div#landlord').append(hideChatBtn);
+    var sendChatBtn = Q("<button class='chat-button'>发送</button>");
+    Q('div#landlord').append(sendChatBtn);
+    var chatInput = Q("<input class='chatInput' style='opacity:0' />");
+    Q('div#landlord').append(chatInput);
+
+    Q(showChatBtn).click(function () {
+        console.log('来聊天吧！');
+        Q(chatInput).show();
+        Q(sendChatBtn).show();
+        Q(showChatBtn).hide();
+        Q(hideChatBtn).show();
+    });
+
+    Q(hideChatBtn).click(function () {
+        console.log('关闭聊天！');
+        Q(chatInput).hide();
+        Q(sendChatBtn).hide();
+        Q(showChatBtn).show();
+        Q(hideChatBtn).hide();
+    });
+}
+
 //loadlive2d("live2d", "http://172.16.1.98:8083/userContent/live2d-core/model/tia/model.json");
 Q(window).load(function () {
     console.log('window load!!!!');
-    //var canvas=Q("<canvas id='mycanvas' width=300px height=500px class='live2d-canvas'></canvas>");
-    //Q('body').append(canvas);
-    //var audio=Q("<audio id='my_audio' class='live2d-audio'></audio>");
-    //Q('body').append(audio);
-    //var button=Q("<button id='Change' class='live2d-button active'>click</button>")
-    //Q('canvas').append(button)
-    //var message=Q("<span class='message'></span>")
-    //Q('body').append(message)
     var div = Q("<div id='landlord'></div>");
     Q('body').append(div);
     var message = Q("<div class='message' style='opacity:0'></div>");
@@ -29,5 +50,8 @@ Q(window).load(function () {
         console.log('隐藏了！');
         Q(div).hide();
     });
+
+    chatUILogic();
+
     InitLive2D();
 });
