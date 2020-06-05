11664e38fc9100281f2c1bc6ffa6682414df1328
hjcenry <HJCenry921209>
2020-06-05 21:30:32 +0800

    update

--- jenkins-live2d-core.js
+++ jenkins-live2d-core.js
@@ -10,7 +10,7 @@ function sendChatToQingyunkeBot(chatContent) {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://127.0.0.1:5000/robot2?key=free&appid=0&msg=" + chatContent,
+        "url": "http://172.16.1.98:5000/robot2?key=free&appid=0&msg=" + chatContent,
         "method": "GET",
         "headers": {
             "cache-control": "no-cache",
